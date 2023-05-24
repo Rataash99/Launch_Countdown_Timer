0ca82320ae004e4d83300f31a84a70b75659f9ae
rahul singh <rohit.singh11299@gmail.com>
2023-05-24 14:44:45 +0530

    js and css added

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
 
 
+let countdown = new Date(`June 8, 2023 00:00:00`);
+
 let timer = setInterval (() => {
-    let date = new Date();
-    let h = date.getHours();
-    let d = date.getDate();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
+    let currDate = new Date().getTime();
+    let dist = countdown - currDate;
+    let d = Math.floor(dist / (1000 * 60 * 60 * 24));
+    let h = Math.floor(dist % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let m = Math.floor(dist % (1000 * 60 * 60) / (1000 * 60));
+    let s = Math.floor(dist % (1000 * 60) / (1000));
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     d = (d < 10) ? "0" + d : d;
-
+    
     days.innerText = d;
     hours.innerText = h;
     minutes.innerText = m;
     seconds.innerHTML =  s;
+
+    if(dist < 0){
+        days.innerText = '00';
+        hours.innerText = '00';
+        minutes.innerText = '00';
+        seconds.innerHTML = '00';
+    }
 })
